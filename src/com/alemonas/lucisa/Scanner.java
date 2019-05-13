e0fc000a1d7a1159129699b0fa0a3786d55f7e02
alejandro <alejandro.monasterios@gmail.com>
2019-05-13 10:54:36 -0400

    WIP: Create Basic Scanner

---
+++ src/com/alemonas/lucisa/Scanner.java
@@ -0,0 +1,81 @@
+package com.alemonas.lucisa;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.List;
+import java.util.Map;
+
+import static com.alemonas.lucisa.TokenType.*;
+
+public class Scanner {
+    private final String source;
+    private final List<Token> tokens = new ArrayList<>();
+    private int start = 0;
+    private int current = 0;
+    private int line = 1;
+
+    Scanner(String source) {
+        this.source = source;
+    }
+
+    List<Token> scanTokens() {
+        while (!isAtEnd()) {
+            // We are ta the beginning of the next lexeme.
+            start = current;
+            scanToken();
+        }
+
+        tokens.add(new Token(EOF, "", null, line));
+        return tokens;
+    }
+
+    private void scanToken() {
+        char c = advance();
+        switch (c) {
+            case '(': addToken(LEFT_PAREN); break;
+            case ')': addToken(RIGHT_PAREN); break;
+            case '{': addToken(LEFT_BRACE); break;
+            case '}': addToken(RIGHT_BRACE); break;
+            case ',': addToken(COMMA); break;
+            case '.': addToken(DOT); break;
+            case '-': addToken(MINUS); break;
+            case '+': addToken(PLUS); break;
+            case ';': addToken(SEMICOLON); break;
+            case '*': addToken(STAR); break;
+            case '!': addToken(match('=') ? BANG_EQUAL : BANG); break;
+            case '=': addToken(match('=') ? EQUAL_EQUAL : EQUAL); break;
+            case '>': addToken(match('=') ? LESS_EQUAL : LESS); break;
+            case '<': addToken(match('=') ? GREATE_EQUAL : GREATER); break;
+            default:
+                Lucisa.error(line, "Unexpected character.");
+            break;
+        }
+    }
+
+    private boolean match(char expected) {
+        if (isAtEnd()) return false;
+        if (source.charAt(current) != expected) return false;
+
+        current++;
+        return true;
+    }
+
+    private boolean isAtEnd() {
+        return current >= source.length();
+    }
+
+    // Consumes the next character in the source file and returns it.
+    private char advance() {
+        current++;
+        return source.charAt(current - 1);
+    }
+
+    private void addToken(TokenType type) {
+        addToken(type, null);
+    }
+
+    private void addToken(TokenType type, Object literal) {
+        String text = source.substring(start, current);
+        tokens.add(new Token(type, text, literal, line));
+    }
+}
---
+++ src/com/alemonas/lucisa/TokenType.java
@@ -0,0 +1,22 @@
+package com.alemonas.lucisa;
+
+public enum TokenType {
+    // Single-character tokens.
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+    COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+    // One or two character tokens.
+    BANG, BANG_EQUAL,
+    EQUAL, EQUAL_EQUAL,
+    GREATER, GREATE_EQUAL,
+    LESS, LESS_EQUAL,
+
+    // Literals
+    IDENTIFIES, STRING, NUMBER,
+
+    // Keywords.
+    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
+    PRINT, RETURN, SUPER, THIS ,TRUE, VAR, WHILE,
+
+    EOF
+}
