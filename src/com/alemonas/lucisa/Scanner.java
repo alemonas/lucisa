0f7c83e4b64466c8f2f410fdab5546aaf50eaf87
alejandro <alejandro.monasterios@gmail.com>
2019-05-13 11:47:14 -0400

    WIP: Scanner | Basic string literals added

--- src/com/alemonas/lucisa/Scanner.java
+++ src/com/alemonas/lucisa/Scanner.java
@@ -62,10 +62,31 @@ public class Scanner {
             case '\n':
                 line++;
                 break;
+            case '"': string(); break;
             default:
                 Lucisa.error(line, "Unexpected character.");
                 break;
         }
+    }
+
+    private void string() {
+        while (peek() != '"' && !isAtEnd()) {
+            if (peek() == '\n') line++;
+            advance();
+        }
+
+        // Unterminated string.
+        if (isAtEnd()) {
+            Lucisa.error(line, "Unterminated string.");
+            return;
+        }
+
+        // The closing ".
+        advance();
+
+        // Trim the surrounding quotes.
+        String value = source.substring(start + 1, current - 1);
+        addToken(STRING, value);
     }
 
     private boolean match(char expected) {
