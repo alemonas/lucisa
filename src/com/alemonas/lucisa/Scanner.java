ebbd225ee475532c82730b3d0ac91fd099830e72
alejandro <alejandro.monasterios@gmail.com>
2019-05-13 12:04:50 -0400

    WIP: Scanner | Basic Number literals added

--- src/com/alemonas/lucisa/Scanner.java
+++ src/com/alemonas/lucisa/Scanner.java
@@ -64,9 +64,29 @@ public class Scanner {
                 break;
             case '"': string(); break;
             default:
-                Lucisa.error(line, "Unexpected character.");
+                if (isDigit(c)) {
+                    number();
+                } else {
+                    Lucisa.error(line, "Unexpected character.");
+                }
                 break;
         }
+    }
+
+    private void number() {
+        while (isDigit(peek())) advance();
+
+        // Look for a fractional part.
+        if (peek() == '.' && isDigit(peekNext())) {
+            // Consume the "."
+            advance();
+
+            while (isDigit(peek())) advance();
+        }
+
+        addToken(NUMBER,
+                Double.parseDouble(source.substring(start, current))
+        );
     }
 
     private void string() {
@@ -102,6 +122,15 @@ public class Scanner {
     private char peek() {
         if (isAtEnd()) return '\0';
         return source.charAt(current);
+    }
+
+    private char peekNext() {
+        if (current + 1 >= source.length()) return '\0';
+        return source.charAt(current + 1);
+    }
+
+    private boolean isDigit(char c) {
+        return c >= '0' && c <= '9';
     }
 
     private boolean isAtEnd() {
