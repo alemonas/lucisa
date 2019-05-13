e4d940855ee9d549b86beedacae899999b2206ae
alejandro <alejandro.monasterios@gmail.com>
2019-05-13 11:38:22 -0400

    WIP: Scanner | basic lexemes added

--- src/com/alemonas/lucisa/Scanner.java
+++ src/com/alemonas/lucisa/Scanner.java
@@ -44,11 +44,27 @@ public class Scanner {
             case '*': addToken(STAR); break;
             case '!': addToken(match('=') ? BANG_EQUAL : BANG); break;
             case '=': addToken(match('=') ? EQUAL_EQUAL : EQUAL); break;
-            case '>': addToken(match('=') ? LESS_EQUAL : LESS); break;
-            case '<': addToken(match('=') ? GREATE_EQUAL : GREATER); break;
+            case '<': addToken(match('=') ? LESS_EQUAL : LESS); break;
+            case '>': addToken(match('=') ? GREATE_EQUAL : GREATER); break;
+            case '/':
+                if (match('/')) {
+                    // A comment goes until the end of the line.
+                    while (peek() != '\n' && !isAtEnd()) advance();
+                } else {
+                    addToken(SLASH);
+                }
+                break;
+            case ' ':
+            case '\r':
+            case '\t':
+                // ignore whitespace
+                break;
+            case '\n':
+                line++;
+                break;
             default:
                 Lucisa.error(line, "Unexpected character.");
-            break;
+                break;
         }
     }
 
@@ -58,6 +74,13 @@ public class Scanner {
 
         current++;
         return true;
+    }
+
+    // similar to advance but does not consume the character
+    // This is called LOOKAHEAD, since only looks at the current unconsumed character.
+    private char peek() {
+        if (isAtEnd()) return '\0';
+        return source.charAt(current);
     }
 
     private boolean isAtEnd() {
