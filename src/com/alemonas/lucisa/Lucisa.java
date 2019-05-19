474491de741934fbdfb70a63f416bfe6617f7f73
alemmr <alejandro.monasterios@gmail.com>
2019-05-18 23:52:38 -0400

    Add error report

--- src/com/alemonas/lucisa/Lucisa.java
+++ src/com/alemonas/lucisa/Lucisa.java
@@ -9,6 +9,7 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lucisa {
+    static boolean hadError = false;
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
             System.out.println("Usage: Lucisa [script]");
@@ -23,6 +24,9 @@ public class Lucisa {
     private static void runFile(String path) throws IOException {
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         run(new String(bytes, Charset.defaultCharset()));
+
+        // indicate an error in the exit coede.
+        if (hadError) System.exit(65);
     }
 
     private static void runPrompt() throws IOException {
@@ -32,6 +36,7 @@ public class Lucisa {
         for (;;) {
             System.out.print("> ");
             run(reader.readLine());
+            hadError = false;
         }
     }
 
@@ -43,5 +48,15 @@ public class Lucisa {
         for (Token token : tokens) {
             System.out.println(token);
         }
+    }
+
+    static void error(int line, String message) {
+        report(line, "", message);
+    }
+
+    private static void report(int line, String where, String message) {
+        System.err.println(
+            "[line "  + line + "] Error" + where + ": " + message);
+        hadError = true;
     }
 }
